ff8faf85724d3619d0a28833219627463a4e8021
Hansjoerg Keser <hansjoerg.kese@idealo.de>
2019-03-21 19:44:45 +0100

    added handling to rest emulator rooted

--- src/test/kotlin/StartPageTests.kt
+++ src/test/kotlin/StartPageTests.kt
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.WebElement
 import pages.StartPage
+import java.io.BufferedReader
 import java.io.File
+import java.io.IOException
+import java.io.InputStreamReader
 import java.util.Arrays.asList
 import java.util.concurrent.TimeUnit
 import java.util.logging.Logger
@@ -72,10 +75,43 @@ class StartPageTests {
 
     @Test
     fun `get screenshot the fast way`() {
+        restartEmulatorRooted()
         updateDataInTable("20", "199", "3980")
         StartPage().tapOnUpdateFab()
         // wait a sec, the app has no loading indicator
         Thread.sleep(1000)
+    }
+
+    /**
+     * like executing "adb root" in terminal
+     * necessary for emulators with the latest android versions including google api services
+     *
+     * does not work with images with play store! does not throw an exception.
+     */
+    private fun restartEmulatorRooted() {
+        val command = mutableListOf<String>()
+        command.add("root")
+        command.add(0, getPathToAdb())
+
+        logger.info("Executing adb root command: $command")
+        val pb = ProcessBuilder()
+        val adbProcess = pb.command(command).start()
+        adbProcess.waitFor()
+        try {
+            BufferedReader(InputStreamReader(adbProcess.inputStream)).readLine()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getPathToAdb(): String {
+        var androidHome = System.getenv("ANDROID_HOME")
+            ?: throw RuntimeException("Environment variable ANDROID_HOME is not set, but is necessary to use adb.")
+
+        if (androidHome.endsWith("/")) {
+            androidHome = "$androidHome/"
+        }
+        return "$androidHome/platform-tools/adb"
     }
 
     /**
@@ -86,6 +122,12 @@ class StartPageTests {
      * note that this column needs an int value, so remove all commas, points, etc. in the parameter string
      * @totalPrice: the total price for all beers;
      * note that this column needs an int value, so remove all commas, points, etc. in the parameter string
+     *
+     * does not work with images with play store, leading to an error like this:
+     * org.openqa.selenium.WebDriverException: An unknown server-side error occurred while processing the command. Original error: Cannot execute the 'sqlite3' shell command.
+     * Original error: Command '/Users/MY.USER/Library/Android/sdk/platform-tools/adb -P 5037 -s emulator-5554 shell sqlite3 /data/data/de.hajo.beermat/databases/beermat-database.db
+     * '"UPDATE Beermat SET amount = 20, price = 199, total_price = 3980"'' exited with code 1. StdOut: .
+     * StdErr: Error: unable to open database "/data/data/de.hajo.beermat/databases/beermat-database.db": unable to open database file
      */
     private fun updateDataInTable(amount: String, price: String, totalPrice: String) {
         val updateTableArgs = asList(
