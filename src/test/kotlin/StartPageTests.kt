d4812b5765cf674e46491e71e7c4740523dc9300
Hansjoerg Keser <hansjoerg.kese@idealo.de>
2019-04-05 17:15:50 +0200

    refactoring

--- src/test/kotlin/StartPageTests.kt
+++ src/test/kotlin/StartPageTests.kt
@@ -1,14 +1,10 @@
-import com.google.common.collect.ImmutableMap
 import driverconfig.AbstractTestSetup
 import driverconfig.MyAppiumDriver.driver
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.WebDriverException
 import pages.StartPage
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.util.Arrays.asList
+import utils.AdbUtils
 import java.util.logging.Logger
 import kotlin.system.measureTimeMillis
 
@@ -46,84 +42,20 @@ class StartPageTests : AbstractTestSetup() {
             )
 
         }
-        logger.info("Duration in milli seconds: [$duration]")
+        logger.info("Duration in milliseconds: [$duration]")
     }
 
     @Test
     fun `get screenshot the fast way`() {
         val duration = measureTimeMillis {
-            restartEmulatorRooted()
-            updateDataInTable("20", "199", "3980")
+            val adbUtils = AdbUtils()
+            adbUtils.restartEmulatorRooted()
+            adbUtils.updateDataInTable("20", "199", "3980")
             StartPage().tapOnUpdateFab()
             // wait a sec, the app has no loading indicator
             Thread.sleep(1000)
         }
-        logger.info("Duration in milli seconds: [$duration]")
-    }
-
-    /**
-     * like executing "adb root" in terminal
-     * necessary for emulators with the latest android versions including google api services
-     *
-     * does not work with images with play store! does not throw an exception.
-     */
-    private fun restartEmulatorRooted() {
-        val command = mutableListOf<String>()
-        command.add("root")
-        command.add(0, getPathToAdb())
-
-        logger.info("Executing adb root command: $command")
-        val pb = ProcessBuilder()
-        val adbProcess = pb.command(command).start()
-        adbProcess.waitFor()
-        try {
-            BufferedReader(InputStreamReader(adbProcess.inputStream)).readLine()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun getPathToAdb(): String {
-        var androidHome = System.getenv("ANDROID_HOME")
-            ?: throw RuntimeException("Environment variable ANDROID_HOME is not set, but is necessary to use adb.")
-
-        if (androidHome.endsWith("/")) {
-            androidHome = "$androidHome/"
-        }
-        return "$androidHome/platform-tools/adb"
-    }
-
-    /**
-     * check all the table names and structure by executing in terminal:
-     * adb shell 'sqlite3 /data/data/de.hajo.beermat/databases/beermat-database.db ".schema"'
-     *
-     * or
-     * check specific table, e.g. favourites:
-     * adb shell 'sqlite3 /data/data/de.hajo.beermat/databases/beermat-database.db "PRAGMA table_info(favorites)"'
-     *
-     * @amount: the amount of beers
-     * @price: the beer price;
-     * note that this column needs an int value, so remove all commas, points, etc. in the parameter string
-     * @totalPrice: the total price for all beers;
-     * note that this column needs an int value, so remove all commas, points, etc. in the parameter string
-     *
-     * does not work with images with play store, leading to an error like this:
-     * org.openqa.selenium.WebDriverException: An unknown server-side error occurred while processing the command. Original error: Cannot execute the 'sqlite3' shell command.
-     * Original error: Command '/Users/MY.USER/Library/Android/sdk/platform-tools/adb -P 5037 -s emulator-5554 shell sqlite3 /data/data/de.hajo.beermat/databases/beermat-database.db
-     * '"UPDATE Beermat SET amount = 20, price = 199, total_price = 3980"'' exited with code 1. StdOut: .
-     * StdErr: Error: unable to open database "/data/data/de.hajo.beermat/databases/beermat-database.db": unable to open database file
-     */
-    private fun updateDataInTable(amount: String, price: String, totalPrice: String) {
-        val updateTableArgs = asList(
-            "/data/data/de.hajo.beermat/databases/beermat-database.db",
-            "\"UPDATE Beermat SET amount = $amount, price = $price, total_price = $totalPrice\""
-        )
-        val manipulateItemPricesCmd = ImmutableMap.of(
-            "command", "sqlite3",
-            "args", updateTableArgs
-        )
-
-        driver.executeScript("mobile: shell", manipulateItemPricesCmd)
+        logger.info("Duration in milliseconds: [$duration]")
     }
 
 }
